326e963073e409a1fbe8d3f58304e906b0f31333
Jonathan Sheahan <jon.sheahan@gmail.com>
2024-08-07 12:38:30 +1000

    Update 1_About.py

--- pages/1_About.py
+++ pages/1_About.py
@@ -3,14 +3,15 @@ st.set_page_config(page_title="About", page_icon="📈")
 
 st.markdown("# About the Data Engineers' Coding Challenge")
 st.divider()
-st.markdown("""The Data Engineers' Coding Challenge was started at the ABS with the following aims:
-            - To encourage staff to develop coding and data skills
-            - To provide fun and interactive challenges where the most experienced coders don't (always) win
-            - To strengthen the community of data professionals across the ABS and encourage collaboration
-            
-            We encourage coders of all abilities to participate in our monthly coding challenges. Submissions can range from 1 to 100+ lines, and involve a creative, fail-fast approach to problem solving. 
-            
-            Typically, results are broadcast live at Friday, 3 PM AEST/ADST, where we go through the submissions received and run the game live. 
+st.markdown("""
+The Data Engineers' Coding Challenge was started at the ABS with the following aims:
+- To encourage staff to develop coding and data skills
+- To provide fun and interactive challenges where the most experienced coders don't (always) win
+- To strengthen the community of data professionals across the ABS and encourage collaboration
 
-            See you in the next one! 
-            """
+We encourage coders of all abilities to participate in our monthly coding challenges. Submissions can range from 1 to 100+ lines, and involve a creative, fail-fast approach to problem solving.
+
+Typically, results are broadcast live at Friday, 3 PM AEST/ADST, where we go through the submissions received and run the game live.
+
+See you in the next one!
+""")
