7bfd413a9f4e9bf92af26aedbff5a7bc3f2dcc8e
Jonathan Sheahan <jon.sheahan@gmail.com>
2024-10-24 20:33:24 +1100

    Update Home.py

--- Home.py
+++ Home.py
@@ -193,7 +193,7 @@ else:
 st.sidebar.divider()
 st.sidebar.header("Current Entrants")
 for entrant, emoji in zip(st.session_state.entrants, st.session_state.emojis):
-    st.sidebar.markdown(f"<div style='font-size: 24px'>- {emoji} {entrant}</div>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<div style='font-size: 24px'>- {emoji}</div> <div style='font-size: 16px'>{entrant}</div>", unsafe_allow_html=True)
 # Contact
 st.sidebar.divider()
 st.sidebar.header("Contact")
